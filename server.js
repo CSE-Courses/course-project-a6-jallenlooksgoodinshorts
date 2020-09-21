8b04e349ad0b6848fc41fe273e03c57c43df3a6d
Xander Covert <xandercovert@Xanders-MBP.fios-router.home>
2020-09-21 11:57:42 -0400

    Cleaned up server.js file

--- server.js
+++ server.js
@@ -8,28 +8,27 @@ const HOST = '0.0.0.0';
 
 //server app directions
 const app = express();
-app.get('/test1', (req, res) => {
-  res.send('Pass test 1');
-});
 
-//Listenting on port 4000
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
-
+//Home Screen
 app.get('/', (req, res) => {
   res.send('Replace with home screen');
 });
-
-
 app.get('/home', (req, res) => {
   res.send('Replace with home screen');
 });
 
+//Login page
 app.get('/login', (req, res) => {
   res.send('Replace with login screen');
 });
-
+//Registration Page
 app.get('/register', (req, res) => {
   res.send('Replace with registration screen');
 });
+
+
+
+//Listenting on port 4000
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
 

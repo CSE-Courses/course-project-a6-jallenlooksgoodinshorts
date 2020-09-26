8a62155d38eb9d805fdd50e01322d789ba5496f1
Xander Covert <xandercovert@Xanders-MacBook-Pro.local>
2020-09-26 16:24:22 -0400

    forms.py created with login and registration forms

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email,
+
+class LoginForm(FlaskForm):
+    email = StringField('firstform', validators=[DataRequired(), Email()])
+    password = PasswordField('lastform', validators=[DataRequired()])
+    submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    firstname = StringField('firstform', validators=[DataRequired()])
+    lastname = StringField('lastform', validators=[DataRequired()])
+    email = StringField('emailform', validators=[DataRequired(), Email()])
+    password = StringField('passform', validators=[DataRequired()])
+
